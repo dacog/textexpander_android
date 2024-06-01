0f4e54fca8bdad7558bf742564d50968f8e74ef3
Diego Carrasco G <557703+dacog@users.noreply.github.com>
2024-06-01 20:46:25 +0200

    app is working and expanding the text of espanso match yml files.

--- app/src/main/java/com/dacotech/textexpanderapp/TextExpanderService.kt
+++ app/src/main/java/com/dacotech/textexpanderapp/TextExpanderService.kt
@@ -4,6 +4,7 @@ import android.accessibilityservice.AccessibilityService
 import android.os.Bundle
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
+import android.util.Log
 
 class TextExpanderService : AccessibilityService() {
 
@@ -21,6 +22,7 @@ class TextExpanderService : AccessibilityService() {
         TriggerRepository.triggers.forEach { match ->
             if (text.endsWith(match.trigger)) {
                 val newText = text.replace(match.trigger, match.replace)
+                Log.d("TextExpander", "Expanding trigger: ${match.trigger} to ${match.replace}")
                 node.performAction(AccessibilityNodeInfo.ACTION_SET_TEXT, Bundle().apply {
                     putCharSequence(
                         AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE,
