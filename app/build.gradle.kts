26c3f7a2cc014f36fc91426ea4bebc3f2e572f92
Diego Carrasco G <557703+dacog@users.noreply.github.com>
2024-06-04 00:10:43 +0200

    Update versionCode and versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.dacotech.textexpanderapp"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
