9a5796196a0abc69e99ba22bd038448807ec57bb
Diego Carrasco G <557703+dacog@users.noreply.github.com>
2024-06-03 21:45:58 +0200

    Add support for multiple triggers as https://espanso.org/docs/matches/basics/#multiple-triggers

--- app/src/main/java/com/dacotech/textexpanderapp/TriggerRepository.kt
+++ app/src/main/java/com/dacotech/textexpanderapp/TriggerRepository.kt
@@ -8,9 +8,16 @@ object TriggerRepository {
     fun loadTriggersFromYAML(yamlContent: String) {
         val yaml = org.yaml.snakeyaml.Yaml()
         val data = yaml.load<Map<String, Any>>(yamlContent)
-        val matches = data["matches"] as List<Map<String, String>>
-        matches.forEach {
-            triggers.add(Match(it["trigger"]!!, it["replace"]!!))
+        val matches = data["matches"] as List<Map<String, Any>>
+        matches.forEach { match ->
+            val replaceText = match["replace"] as? String ?: ""
+            if ("triggers" in match) {
+                (match["triggers"] as List<String>).forEach { trigger ->
+                    triggers.add(Match(trigger, replaceText))
+                }
+            } else if ("trigger" in match) {
+                triggers.add(Match(match["trigger"] as String, replaceText))
+            }
         }
     }
 }
